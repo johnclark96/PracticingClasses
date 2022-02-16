b2a38d2623cd341579686234ed2172596cd34959
JClark <mlitvinkox96@gmail.com>
2022-02-16 18:56:48 -0500

    Initial Code Setup Main Function

---
+++ PracticingClasses/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class FirstClass { //Class Format
+public:  //Accessor Specifier
+	int firstNum;
+	string firstString;
+};
+
+int main() {
+	
+	cout << "Hello World!!!" << endl; //cout: outputs endl: end line
+
+	system("PAUSE");
+	return 0;
+}
