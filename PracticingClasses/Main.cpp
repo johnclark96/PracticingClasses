c8f87d1760d90df95c9d54b604e0b8864f669807
JClark <mlitvinkox96@gmail.com>
2022-02-16 19:07:48 -0500

    Minor Code testing Class

--- PracticingClasses/Main.cpp
+++ PracticingClasses/Main.cpp
@@ -4,13 +4,23 @@ using namespace std;
 
 class FirstClass { //Class Format
 public:  //Accessor Specifier
-	int firstNum;
-	string firstString;
+	int firstNum = 0;
+	string firstString = "";
 };
 
 int main() {
 	
 	cout << "Hello World!!!" << endl; //cout: outputs endl: end line
+
+	FirstClass myClass; //Designated myClass to be FirstClass (Like a variable char a)
+	
+	cout << myClass.firstNum << " and " << myClass.firstString << endl;
+	cout << endl;
+
+	myClass.firstNum = 2;
+	myClass.firstString = "A String";
+
+	cout << myClass.firstNum << " and " << myClass.firstString << endl;
 
 	system("PAUSE");
 	return 0;
